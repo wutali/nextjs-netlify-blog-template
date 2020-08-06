96bb325683a1d80c40505f7ec23d39dfdcc3c2a9
wutali <email@wuta.li>
2020-08-06 16:30:02 +0900

    Clean up components

---
+++ components/Layout.tsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import Navigation from "./Navigation";
+
+type Props = {
+  children: React.ReactNode;
+};
+export default function Layout({ children }: Props) {
+  return (
+    <div>
+      <Head>
+        <meta charSet="utf-8" />
+        <title></title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta property="og:title" content="" />
+        <meta property="og:type" content="" />
+        <meta property="og:url" content="" />
+        <meta property="og:image" content="" />
+
+        <link rel="manifest" href="site.webmanifest" />
+        <link rel="apple-touch-icon" href="icon.png" />
+
+        <meta name="theme-color" content="#fafafa" />
+      </Head>
+      <Navigation />
+      <main>{children}</main>
+    </div>
+  );
+}
