68247d25a6784cb8eaa2eb55b43947d8117b868e
wutali <email@wuta.li>
2020-08-07 14:52:23 +0900

    Add normalize css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import '../styles/global.css'
-import { AppProps } from 'next/app'
+import "normalize.css";
+import "../styles/global.css";
+import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
