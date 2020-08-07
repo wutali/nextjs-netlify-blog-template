cad751121f7f05e159f041a56c49b12e679ddee0
wutali <email@wuta.li>
2020-08-07 13:35:04 +0900

    Add meta tag

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Navigation from "./Navigation";
+import settings from "../settings.yml";
 
 type Props = {
   children: React.ReactNode;
@@ -9,14 +10,14 @@ export default function Layout({ children }: Props) {
     <div>
       <Head>
         <meta charSet="utf-8" />
-        <title></title>
-        <meta name="description" content="" />
+        <title>{settings.site_title}</title>
+        <meta name="description" content={settings.site_description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <meta property="og:title" content="" />
+        <meta property="og:title" content={settings.site_title} />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="" />
-        <meta property="og:image" content="" />
+        <meta property="og:url" content={settings.url} />
+        <meta property="og:image" content={settings.url + "/og_image.png"} />
 
         <link rel="manifest" href="site.webmanifest" />
         <link rel="apple-touch-icon" href="icon.png" />
--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   exportTrailingSlash: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: /\.yml/,
+      type: "json",
+      use: "yaml-loader",
+    });
+    return config;
+  },
 };
