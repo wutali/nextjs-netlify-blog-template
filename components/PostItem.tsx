a72bc1a016dd2dbad69e66f8e8176c6460b9e8be
wutali <email@wuta.li>
2020-08-12 16:09:16 +0900

    List pages

--- components/PostItem.tsx
+++ components/PostItem.tsx
@@ -1,22 +1,28 @@
 import { Post } from "../lib/posts";
 import Date from "./Date";
+import Link from "next/link";
 
 type Props = {
   post: Post;
 };
 export default function ({ post }: Props) {
   return (
-    <div>
-      <Date dateString={post.date} />
-      <h2>{post.title}</h2>
-      <style jsx>
-        {`
-          h2 {
-            margin: 0;
-            font-weight: 500;
-          }
-        `}
-      </style>
-    </div>
+    <Link href={"/posts/" + post.slug}>
+      <a>
+        <Date dateString={post.date} />
+        <h2>{post.title}</h2>
+        <style jsx>
+          {`
+            a {
+              color: #111;
+            }
+            h2 {
+              margin: 0;
+              font-weight: 500;
+            }
+          `}
+        </style>
+      </a>
+    </Link>
   );
 }
--- lib/posts.ts
+++ lib/posts.ts
@@ -4,34 +4,36 @@ import matter from "gray-matter";
 import remark from "remark";
 import html from "remark-html";
 
-const postsDirectory = path.join(process.cwd(), "posts");
+const postsDirectory = path.join(process.cwd(), "pages/posts");
 
 export type Post = {
   date: string;
   title: string;
-  id: string;
+  slug: string;
 };
 
 export function getSortedPostsData(): Post[] {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get slug
-    const id = fileName.replace(/\.md$/, "");
+  const allPostsData = fileNames
+    .filter((it) => it.endsWith(".mdx"))
+    .map((fileName) => {
+      // Remove ".md" from file name to get slug
+      const slug = fileName.replace(/\.mdx$/, "");
 
-    // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, "utf8");
+      // Read markdown file as string
+      const fullPath = path.join(postsDirectory, fileName);
+      const fileContents = fs.readFileSync(fullPath, "utf8");
 
-    // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
+      // Use gray-matter to parse the post metadata section
+      const matterResult = matter(fileContents);
 
-    // Combine the data with the id
-    return {
-      id,
-      ...(matterResult.data as { date: string; title: string }),
-    };
-  });
+      // Combine the data with the id
+      return {
+        slug,
+        ...(matterResult.data as { date: string; title: string }),
+      };
+    });
   // Sort posts by date
   return allPostsData.sort((a, b) => {
     if (a.date < b.date) {
@@ -40,36 +42,4 @@ export function getSortedPostsData(): Post[] {
       return -1;
     }
   });
-}
-
-export function getAllPostIds() {
-  const fileNames = fs.readdirSync(postsDirectory);
-  return fileNames.map((fileName) => {
-    return {
-      params: {
-        slug: fileName.replace(/\.md$/, ""),
-      },
-    };
-  });
-}
-
-export async function getPostData(slug: string) {
-  const fullPath = path.join(postsDirectory, `${slug}.md`);
-  const fileContents = fs.readFileSync(fullPath, "utf8");
-
-  // Use gray-matter to parse the post metadata section
-  const matterResult = matter(fileContents);
-
-  // Use remark to convert markdown into HTML string
-  const processedContent = await remark()
-    .use(html)
-    .process(matterResult.content);
-  const contentHtml = processedContent.toString();
-
-  // Combine the data with the id and contentHtml
-  return {
-    slug,
-    contentHtml,
-    ...(matterResult.data as { date: string; title: string }),
-  };
 }
