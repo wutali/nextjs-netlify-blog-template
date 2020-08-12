c222a70599b0a8caef8c535784d0d2c800af9258
wutali <email@wuta.li>
2020-08-12 22:00:03 +0900

    Add two test cases

--- src/__tests__/pagination.test.ts
+++ src/__tests__/pagination.test.ts
@@ -13,4 +13,26 @@ test("excerpt starting and ending page numbers", () => {
   ]);
 });
 
+test("excerpt ending page numbers", () => {
+  const pagination = generatePagination(2, 8);
+  expect(pagination).toStrictEqual([
+    { page: 1, current: false, excerpt: false },
+    { page: 2, current: true, excerpt: false },
+    { page: 3, current: false, excerpt: false },
+    { page: null, current: false, excerpt: true },
+    { page: 8, current: false, excerpt: false },
+  ]);
+});
+
+test("excerpt starting page numbers", () => {
+  const pagination = generatePagination(7, 8);
+  expect(pagination).toStrictEqual([
+    { page: 1, current: false, excerpt: false },
+    { page: null, current: false, excerpt: true },
+    { page: 6, current: false, excerpt: false },
+    { page: 7, current: true, excerpt: false },
+    { page: 8, current: false, excerpt: false },
+  ]);
+});
+
 export {};
