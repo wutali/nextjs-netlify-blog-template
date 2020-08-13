174afae93d3ffd1eb8893e6aa97845831520f3bc
wutali <email@wuta.li>
2020-08-13 15:56:50 +0900

    Style tag post list

--- src/components/TagPostList.tsx
+++ src/components/TagPostList.tsx
@@ -14,44 +14,52 @@ type Props = {
 };
 export default function TagPostList({ posts, tag, pagination }: Props) {
   return (
-    <div>
-      <div className={"container"}>
-        <h1>All Posts / {tag.name}</h1>
-        <ul>
-          {posts.map((it, i) => (
-            <li key={i}>
-              <PostItem post={it} />
-            </li>
-          ))}
-        </ul>
-        <Pagination
-          current={pagination.current}
-          pages={pagination.pages}
-          link={{
-            href: () => "/posts/tags/[[...slug]]",
-            as: (page) =>
-              page === 1
-                ? "/posts/tags/" + tag.slug
-                : `/posts/tags/${tag.slug}/${page}`,
-          }}
-        />
-      </div>
+    <div className={"container"}>
+      <h1>
+        All Posts / <span>{tag.name}</span>
+      </h1>
+      <ul>
+        {posts.map((it, i) => (
+          <li key={i}>
+            <PostItem post={it} />
+          </li>
+        ))}
+      </ul>
+      <Pagination
+        current={pagination.current}
+        pages={pagination.pages}
+        link={{
+          href: () => "/posts/tags/[[...slug]]",
+          as: (page) =>
+            page === 1
+              ? "/posts/tags/" + tag.slug
+              : `/posts/tags/${tag.slug}/${page}`,
+        }}
+      />
       <style jsx>
         {`
           .container {
-            padding: 0 20px;
             margin: 0 auto;
             max-width: 1200px;
             width: 100%;
             padding: 0 20px;
+            display: flex;
+            flex-direction: column;
           }
           h1 {
             padding-left: 30px;
             margin: 0 0 2rem;
+            font-weight: 100;
+            color: #9b9b9b;
+          }
+          h1 span {
+            font-weight: bold;
+            color: #222;
           }
           ul {
             margin: 0;
             padding-left: 30px;
+            flex: 1;
           }
           li {
             list-style: none;
