44a36415743bb717101f7df3b9b4de7b8cc2ff4d
wutali <email@wuta.li>
2020-08-14 07:57:17 +0900

    Add syntax highlighting

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const withMdxEnhanced = require("next-mdx-enhanced");
+const rehypePrism = require("@mapbox/rehype-prism");
 
 module.exports = withMdxEnhanced({
   layoutPath: "src/layouts",
   defaultLayout: true,
+  rehypePlugins: [rehypePrism],
 })({
   trailingSlash: true,
   pageExtensions: ["mdx", "tsx"],
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -8,6 +8,7 @@ type Props = {
   date: string;
 };
 export default function Index({ title, date }: Props) {
+  console.log(styles);
   return ({ children: content }) => {
     return (
       <Layout>
@@ -27,6 +28,106 @@ export default function Index({ title, date }: Props) {
             h1 {
               margin: 0 0 0.5rem;
               font-size: 2.25rem;
+            }
+          `}
+        </style>
+        <style global jsx>
+          {`
+            /* Syntax highlighting */
+            .token.comment,
+            .token.prolog,
+            .token.doctype,
+            .token.cdata,
+            .token.plain-text {
+              color: #6a737d;
+            }
+
+            .token.atrule,
+            .token.attr-value,
+            .token.keyword,
+            .token.operator {
+              color: #d73a49;
+            }
+
+            .token.property,
+            .token.tag,
+            .token.boolean,
+            .token.number,
+            .token.constant,
+            .token.symbol,
+            .token.deleted {
+              color: #22863a;
+            }
+
+            .token.selector,
+            .token.attr-name,
+            .token.string,
+            .token.char,
+            .token.builtin,
+            .token.inserted {
+              color: #032f62;
+            }
+
+            .token.function,
+            .token.class-name {
+              color: #6f42c1;
+            }
+
+            /* language-specific */
+
+            /* JSX */
+            .language-jsx .token.punctuation,
+            .language-jsx .token.tag .token.punctuation,
+            .language-jsx .token.tag .token.script,
+            .language-jsx .token.plain-text {
+              color: #24292e;
+            }
+
+            .language-jsx .token.tag .token.attr-name {
+              color: #6f42c1;
+            }
+
+            .language-jsx .token.tag .token.class-name {
+              color: #005cc5;
+            }
+
+            .language-jsx .token.tag .token.script-punctuation,
+            .language-jsx .token.attr-value .token.punctuation:first-child {
+              color: #d73a49;
+            }
+
+            .language-jsx .token.attr-value {
+              color: #032f62;
+            }
+
+            .language-jsx span[class="comment"] {
+              color: pink;
+            }
+
+            /* HTML */
+            .language-html .token.tag .token.punctuation {
+              color: #24292e;
+            }
+
+            .language-html .token.tag .token.attr-name {
+              color: #6f42c1;
+            }
+
+            .language-html .token.tag .token.attr-value,
+            .language-html
+              .token.tag
+              .token.attr-value
+              .token.punctuation:not(:first-child) {
+              color: #032f62;
+            }
+
+            /* CSS */
+            .language-css .token.selector {
+              color: #6f42c1;
+            }
+
+            .language-css .token.property {
+              color: #005cc5;
             }
           `}
         </style>
